8e8760a29f5c53310e718d1fd700666c4a3ecbfb
most-Toxa <anatohey0@gmail.com>
2020-12-23 18:53:50 +0200

    first commit

---
+++ src/main/java/net/dev4any1/App.java
@@ -0,0 +1,13 @@
+package net.dev4any1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
